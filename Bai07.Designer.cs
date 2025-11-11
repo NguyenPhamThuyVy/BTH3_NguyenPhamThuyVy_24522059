db272749cc280e0742d23dc0c08e9faecfc52d74
NguyenPhamThuyVy <24522059@gm.uit.edu.vn>
2025-11-11 19:20:17 +0700

    Add files via upload

---
+++ Bai07.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace BTH3_BT7
+{
+    partial class Bai07
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            btn1 = new Button();
+            btn6 = new Button();
+            btn11 = new Button();
+            btn12 = new Button();
+            btn7 = new Button();
+            btn2 = new Button();
+            btn13 = new Button();
+            btn8 = new Button();
+            btn3 = new Button();
+            btn14 = new Button();
+            btn9 = new Button();
+            btn4 = new Button();
+            btn15 = new Button();
+            btn10 = new Button();
+            btn5 = new Button();
+            label2 = new Label();
+            txtDisplay = new TextBox();
+            btnChon = new Button();
+            btnHuy = new Button();
+            btnKT = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.DarkOrange;
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(552, 72);
+            label1.TabIndex = 0;
+            label1.Text = "MÀN ẢNH";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // btn1
+            // 
+            btn1.Cursor = Cursors.Hand;
+            btn1.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn1.Location = new Point(27, 105);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(80, 81);
+            btn1.TabIndex = 1;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += Ghe_Click;
+            // 
+            // btn6
+            // 
+            btn6.Cursor = Cursors.Hand;
+            btn6.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn6.Location = new Point(27, 204);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(80, 81);
+            btn6.TabIndex = 2;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += Ghe_Click;
+            // 
+            // btn11
+            // 
+            btn11.Cursor = Cursors.Hand;
+            btn11.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn11.Location = new Point(27, 301);
+            btn11.Name = "btn11";
+            btn11.Size = new Size(80, 81);
+            btn11.TabIndex = 3;
+            btn11.Text = "11";
+            btn11.UseVisualStyleBackColor = true;
+            btn11.Click += Ghe_Click;
+            // 
+            // btn12
+            // 
+            btn12.Cursor = Cursors.Hand;
+            btn12.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn12.Location = new Point(131, 301);
+            btn12.Name = "btn12";
+            btn12.Size = new Size(80, 81);
+            btn12.TabIndex = 6;
+            btn12.Text = "12";
+            btn12.UseVisualStyleBackColor = true;
+            btn12.Click += Ghe_Click;
+            // 
+            // btn7
+            // 
+            btn7.Cursor = Cursors.Hand;
+            btn7.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn7.Location = new Point(131, 204);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(80, 81);
+            btn7.TabIndex = 5;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += Ghe_Click;
+            // 
+            // btn2
+            // 
+            btn2.Cursor = Cursors.Hand;
+            btn2.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn2.Location = new Point(131, 105);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(80, 81);
+            btn2.TabIndex = 4;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += Ghe_Click;
+            // 
+            // btn13
+            // 
+            btn13.Cursor = Cursors.Hand;
+            btn13.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn13.Location = new Point(235, 301);
+            btn13.Name = "btn13";
+            btn13.Size = new Size(80, 81);
+            btn13.TabIndex = 9;
+            btn13.Text = "13";
+            btn13.UseVisualStyleBackColor = true;
+            btn13.Click += Ghe_Click;
+            // 
+            // btn8
+            // 
+            btn8.Cursor = Cursors.Hand;
+            btn8.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn8.Location = new Point(235, 204);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(80, 81);
+            btn8.TabIndex = 8;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += Ghe_Click;
+            // 
+            // btn3
+            // 
+            btn3.Cursor = Cursors.Hand;
+            btn3.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn3.Location = new Point(235, 105);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(80, 81);
+            btn3.TabIndex = 7;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += Ghe_Click;
+            // 
+            // btn14
+            // 
+            btn14.Cursor = Cursors.Hand;
+            btn14.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn14.Location = new Point(337, 301);
+            btn14.Name = "btn14";
+            btn14.Size = new Size(80, 81);
+            btn14.TabIndex = 12;
+            btn14.Text = "14";
+            btn14.UseVisualStyleBackColor = true;
+            btn14.Click += Ghe_Click;
+            // 
+            // btn9
+            // 
+            btn9.Cursor = Cursors.Hand;
+            btn9.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn9.Location = new Point(337, 204);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(80, 81);
+            btn9.TabIndex = 11;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += Ghe_Click;
+            // 
+            // btn4
+            // 
+            btn4.Cursor = Cursors.Hand;
+            btn4.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn4.Location = new Point(337, 105);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(80, 81);
+            btn4.TabIndex = 10;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += Ghe_Click;
+            // 
+            // btn15
+            // 
+            btn15.Cursor = Cursors.Hand;
+            btn15.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn15.Location = new Point(441, 301);
+            btn15.Name = "btn15";
+            btn15.Size = new Size(80, 81);
+            btn15.TabIndex = 15;
+            btn15.Text = "15";
+            btn15.UseVisualStyleBackColor = true;
+            btn15.Click += Ghe_Click;
+            // 
+            // btn10
+            // 
+            btn10.Cursor = Cursors.Hand;
+            btn10.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn10.Location = new Point(441, 204);
+            btn10.Name = "btn10";
+            btn10.Size = new Size(80, 81);
+            btn10.TabIndex = 14;
+            btn10.Text = "10";
+            btn10.UseVisualStyleBackColor = true;
+            btn10.Click += Ghe_Click;
+            // 
+            // btn5
+            // 
+            btn5.Cursor = Cursors.Hand;
+            btn5.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn5.Location = new Point(441, 105);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(80, 81);
+            btn5.TabIndex = 13;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += Ghe_Click;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(27, 422);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 32);
+            label2.TabIndex = 16;
+            label2.Text = "Thành Tiền:";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // txtDisplay
+            // 
+            txtDisplay.Location = new Point(142, 420);
+            txtDisplay.Multiline = true;
+            txtDisplay.Name = "txtDisplay";
+            txtDisplay.Size = new Size(379, 34);
+            txtDisplay.TabIndex = 17;
+            // 
+            // btnChon
+            // 
+            btnChon.Location = new Point(99, 472);
+            btnChon.Name = "btnChon";
+            btnChon.Size = new Size(94, 39);
+            btnChon.TabIndex = 18;
+            btnChon.Text = "Chọn";
+            btnChon.UseVisualStyleBackColor = true;
+            btnChon.Click += btnChon_Click;
+            // 
+            // btnHuy
+            // 
+            btnHuy.Location = new Point(235, 472);
+            btnHuy.Name = "btnHuy";
+            btnHuy.Size = new Size(94, 39);
+            btnHuy.TabIndex = 19;
+            btnHuy.Text = "Hủy bỏ";
+            btnHuy.UseVisualStyleBackColor = true;
+            btnHuy.Click += btnHuy_Click;
+            // 
+            // btnKT
+            // 
+            btnKT.Location = new Point(373, 472);
+            btnKT.Name = "btnKT";
+            btnKT.Size = new Size(94, 39);
+            btnKT.TabIndex = 20;
+            btnKT.Text = "Kết thúc";
+            btnKT.UseVisualStyleBackColor = true;
+            btnKT.Click += btnKT_Click;
+            // 
+            // Bai07
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(548, 531);
+            Controls.Add(btnKT);
+            Controls.Add(btnHuy);
+            Controls.Add(btnChon);
+            Controls.Add(txtDisplay);
+            Controls.Add(label2);
+            Controls.Add(btn15);
+            Controls.Add(btn10);
+            Controls.Add(btn5);
+            Controls.Add(btn14);
+            Controls.Add(btn9);
+            Controls.Add(btn4);
+            Controls.Add(btn13);
+            Controls.Add(btn8);
+            Controls.Add(btn3);
+            Controls.Add(btn12);
+            Controls.Add(btn7);
+            Controls.Add(btn2);
+            Controls.Add(btn11);
+            Controls.Add(btn6);
+            Controls.Add(btn1);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Bai07";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "BÁN VÉ RẠP CHIẾU PHIM";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button btn1;
+        private Button btn6;
+        private Button btn11;
+        private Button btn12;
+        private Button btn7;
+        private Button btn2;
+        private Button btn13;
+        private Button btn8;
+        private Button btn3;
+        private Button btn14;
+        private Button btn9;
+        private Button btn4;
+        private Button btn15;
+        private Button btn10;
+        private Button btn5;
+        private Label label2;
+        private TextBox txtDisplay;
+        private Button btnChon;
+        private Button btnHuy;
+        private Button btnKT;
+    }
+}
---
+++ Bai07.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+namespace BTH3_BT7
+{
+    public partial class Bai07 : Form
+    {
+        // Mảng ghe
+        Button[] ghe = new Button[15];
+        // Danh sách các ghế đang chọn
+        List<Button> DangChon = new List<Button>();
+        // Giá vé theo từng hàng
+        const int giaA = 5000, giaB = 6500, giaC = 8000;
+        public Bai07()
+        {
+            InitializeComponent();
+            this.Load += Bai07_Load;
+        }
+        private void Bai07_Load(object sender, EventArgs e)
+        {
+            ghe[0] = btn1; ghe[1] = btn2; ghe[2] = btn3; ghe[3] = btn4; ghe[4] = btn5;
+            ghe[5] = btn6; ghe[6] = btn7; ghe[7] = btn8; ghe[8] = btn9; ghe[9] = btn10;
+            ghe[10] = btn11; ghe[11] = btn12; ghe[12] = btn13; ghe[13] = btn14; ghe[14] = btn15;
+            
+            for (int i = 0; i < 15; i++)
+            {
+                if (i < 5) ghe[i].Tag = "A";
+                else if (i < 10) ghe[i].Tag = "B";
+                else ghe[i].Tag = "C";
+
+                ghe[i].BackColor = Color.White;
+            }
+            txtDisplay.Text = "0";
+        }
+        // Xử lý sự kiện khi nhấn vào ghế
+        private void Ghe_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            // Nếu ghế màu vàng nghĩa là đã được bán
+            if (btn.BackColor == Color.Yellow)
+            {
+                MessageBox.Show($"Ghế {btn.Text} đã được bán!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            // Nếu ghế màu xanh nghĩa là đang chọn
+            if (btn.BackColor == Color.Blue)
+            {
+                btn.BackColor = Color.White;
+                DangChon.Remove(btn);
+            }
+            // Nếu ghế là màu trắng nghĩa là chưa chọn
+            else if (btn.BackColor == Color.White)
+            {
+                btn.BackColor = Color.Blue;
+                DangChon.Add(btn);
+            }
+        }
+        // Xử lý sự kiện cho nút Chọn
+        private void btnChon_Click(object sender, EventArgs e)
+        {
+            int T1 = 0;
+            foreach (Button b in DangChon)
+            {
+                string hang = b.Tag.ToString();
+                if (hang == "A") T1 += giaA;
+                else if (hang == "B") T1 += giaB;
+                else T1 += giaC;
+
+                b.BackColor = Color.Yellow;
+            }
+            txtDisplay.Text = T1.ToString();
+            DangChon.Clear();
+        }
+        // Xử lý sự kiện cho nút Hủy
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            foreach (Button b in DangChon)
+            {
+                b.BackColor = Color.White;
+            }
+            txtDisplay.Text = "0";
+            DangChon.Clear();
+        }
+        // Xử lý sự kiện cho nút Kết thúc
+        private void btnKT_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
